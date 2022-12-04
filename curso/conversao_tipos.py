8a5b7ed0f37707856eecea326c9692b228d0e645
lucas-brisolla <99052177+lucas-brisolla@users.noreply.github.com>
2022-12-04 15:28:54 -0300

    fim de mais um modulo

---
+++ curso/conversao_tipos.py
@@ -0,0 +1,12 @@
+print(int(1.9548895))
+print(int("56"))
+print(float("15.12"))
+print(float(122))
+
+valor = 15.12
+valor_str = str(valor)
+print(valor, type(valor), type(valor_str))
+
+print(100 / 2)
+print(100 // 2)
+
