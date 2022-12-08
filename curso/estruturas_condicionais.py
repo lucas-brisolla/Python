f6d2ffdef45e5007fde9e9bafe76f4e32b35e9eb
lucas-brisolla <99052177+lucas-brisolla@users.noreply.github.com>
2022-12-08 09:56:12 -0300

    Programa cabuloso!

---
+++ curso/estruturas_condicionais.py
@@ -0,0 +1,55 @@
+programas = int(input("Escolha qual programa deseja rodar [1]\n[2]\n[3] "))
+
+
+# CNH -----------------------------------------------------------------------
+if programas == 1:
+    MAIOR_IDADE_BRASIL = 18
+    idade = int(input("Informe sua idade: "))
+
+    tempo_restante = MAIOR_IDADE_BRASIL - idade
+
+    if idade >= MAIOR_IDADE_BRASIL:
+        print("Você esta apto a tirar sua CNH!")
+    else:
+        print(f"Infelizmente Você ainda não tem a idade minima para tirar a CNH, ainda faltam {tempo_restante} anos para você estar apto.")
+
+#Banco simples --------------------------------------------------------------
+elif programas == 2:
+    opcao = int(input("Informe uma opção: [1] sacar \n[2] extrato "))
+
+    if opcao == 1:
+        valor = float(input("Informe o valor para saque: "))
+    elif opcao == 2:
+        print("Exibindo o extrato ... ")
+    else:
+        print("Opção inválida!!!")
+
+# if aninhado ---------------------------------------------------------------
+elif programas == 3:
+    contas = int(input("Selecione uma opção de conta conta normal [1] \n conta universitaria [2] "))
+
+
+    saldo = 10000
+    saque = float(input("Informe o valor do saque: "))
+    cheque_especial = 1000
+
+    conta_normal = 1
+
+    conta_universitaria = 0
+
+    if contas == 1:
+        if conta_normal == True:
+            if saldo >= saque:
+                print("Saque realizado com sucesso!")
+            elif saque <= (saldo + cheque_especial):
+                print("Saque realizado com uso do cheque especial")
+            else:
+                print("Saldo insuficiente!")
+    elif contas == 2:
+        if conta_universitaria == False:
+            if saldo >= saque:
+                print("Saque realizado com sucesso")
+            else:
+                print("Saldo insuficiente!")
+else:
+    print("Opção inválida!")
--- curso/identacao_blocos.py
+++ curso/identacao_blocos.py
@@ -3,6 +3,12 @@ def sacar(valor):
 
     if saldo >= valor:
         print("valor sacado!")
+        print("retire o seu dinheiro na boca do caixa.")
 
+    print("Obrigado por ser nosso cliente, tenha um bom dia!")
 
-sacar(100)
+def depositar(valor):
+    saldo = 500
+    saldo += valor
+
+sacar(1000)
